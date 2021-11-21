2aca5b4c5ce8ac5022627d9017db31dfc4fba21d
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-11-20 22:32:27 -0300

    simple declaration of TabView

--- SwiftUI-Navigation/SwiftUI-Navigation/ContentView.swift
+++ SwiftUI-Navigation/SwiftUI-Navigation/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selection = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            Text("One")
+                .tabItem {
+                    Text("One")
+                }
+                .tag(1)
+            Text("Two")
+                .tabItem {
+                    Text("Two")
+                }
+                .tag(2)
+            Text("Three")
+                .tabItem {
+                    Text("Three")
+                }
+                .tag(3)
+        }
+        .tabViewStyle(DefaultTabViewStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(selection: 2)
     }
 }

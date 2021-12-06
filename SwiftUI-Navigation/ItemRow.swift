07922df629b2f26c9ceecb1dde05207472f29f2c
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-12-05 21:45:56 -0300

    added a edit feature to edit item

--- SwiftUI-Navigation/ItemRow.swift
+++ SwiftUI-Navigation/ItemRow.swift
@@ -38,6 +38,19 @@ final class ItemRowViewModel: Identifiable, ObservableObject {
         self.route = nil
         onDelete()
     }
+    
+    func editButtonTapped() {
+        self.route = .edit(self.item)
+    }
+    
+    func edit(item: Item) {
+        self.item = item
+        self.route = nil
+    }
+    
+    func cancelButtonTapped() {
+        self.route = nil
+    }
 }
 
 struct ItemRowView: View {
@@ -64,6 +77,11 @@ struct ItemRowView: View {
                     .foregroundColor(color.swiftUIColor)
                     .border(Color.black, width: 1)
             }
+            
+            Button(action: viewModel.editButtonTapped) {
+                Image(systemName: "pencil")
+            }
+            .padding(.leading)
             
             Button(action: viewModel.deleteButtonTapped) {
                 Image(systemName: "trash.fill")
@@ -95,7 +113,21 @@ struct ItemRowView: View {
                 }
             )
         ) { $item in
-            ItemView(item: $item)
+            NavigationView {
+                ItemView(item: $item)
+                    .navigationTitle("Edit")
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel", action: viewModel.cancelButtonTapped)
+                        }
+                        
+                        ToolbarItem(placement: .primaryAction) {
+                            Button("Save") {
+                                self.viewModel.edit(item: item)
+                            }
+                        }
+                    }
+            }
         }
         .onTapGesture {
 //            self.viewModel.itemToAdd = item

c7d138870d62bdf48328df0d488b757c0f45b50b
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-12-12 14:31:54 -0300

    some comments added to duplicate item

--- SwiftUI-NavigationTests/SwiftUI_NavigationTests.swift
+++ SwiftUI-NavigationTests/SwiftUI_NavigationTests.swift
@@ -52,25 +52,35 @@ final class SwiftUI_NavigationTests: XCTestCase {
     }
     
     func testDuplicateItem() {
+        // There is an Item
         let item: Item = .init(name: "Keyboard", color: .red, status: .inStock(quantity: 1))
+        // And a viewModel with this item
         let viewModel = InventoryViewModel(
             inventory: [
                 .init(item: item)
             ]
         )
         
+        // then use types on duplicate button
         viewModel.inventory[0].duplicateButtonTapped()
 //        XCTAssertEqual(viewModel.inventory[0].route, .duplicate(item))
+        // and then route variable for that particular ItemRowViewModel must be .duplicate(Itm)
         XCTAssertNotNil(
             (/ItemRowViewModel.Route.duplicate).extract(from: try XCTUnwrap(viewModel.inventory[0].route))
         )
         
+        // now i have a duplicated item called dup that comes from our previous item
         let dup = item.duplicate()
+        // and user hits on duplicate button
         viewModel.inventory[0].duplicate(item: dup)
         
+        // and now we have two inventories
         XCTAssertEqual(viewModel.inventory.count, 2)
+        // and first item is our previous Item
         XCTAssertEqual(viewModel.inventory[0].item, item)
+        // and second item is our duplicated item dub
         XCTAssertEqual(viewModel.inventory[1].item, dup)
+        // and our route for first item doesn't exists anymore
         XCTAssertNil(viewModel.inventory[0].route)
     }
 }

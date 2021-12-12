58f6b7c454538852a046038bce8b40153e54def3
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-12-12 12:48:25 -0300

    added some comments

--- SwiftUI-NavigationTests/SwiftUI_NavigationTests.swift
+++ SwiftUI-NavigationTests/SwiftUI_NavigationTests.swift
@@ -11,16 +11,23 @@ import XCTest
 final class SwiftUI_NavigationTests: XCTestCase {
     func testAddItem() throws {
         let viewModel = InventoryViewModel()
+        
+        // When addButtonTapped is executed
         viewModel.addButtonTapped()
         
+        // There must be an Item in itemToAdd
         XCTAssertNotNil(viewModel.itemToAdd)
         
         let itemToAdd = try XCTUnwrap(viewModel.itemToAdd)
         
+        // When an Item isso added throw .add(item:)
         viewModel.add(item: itemToAdd)
         
+        // It must be nothing in itemToAdd
         XCTAssertNil(viewModel.itemToAdd)
+        // The inventonry count must be one
         XCTAssertEqual(viewModel.inventory.count, 1)
+        // and the inventory item must be equal to itemToAdd constant
         XCTAssertEqual(viewModel.inventory[0].item, itemToAdd)
     }
     
@@ -32,11 +39,9 @@ final class SwiftUI_NavigationTests: XCTestCase {
         )
         
         viewModel.inventory[0].deleteButtonTapped()
-        
         XCTAssertEqual(viewModel.inventory[0].route, .deleteAlert)
         
         viewModel.inventory[0].deleteConfirmationButtonTapped()
-        
         XCTAssertEqual(viewModel.inventory.count, 0)
     }
 }

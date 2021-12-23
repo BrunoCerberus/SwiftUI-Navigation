fff2c384448bd1221ddfd54743dfc032d7bbac8a
Bruno Lopes <bruno.guitarpro@gmail.com>
2021-12-22 23:38:02 -0300

    added route parameter explicitly

--- SwiftUI-Navigation/SwiftUI_NavigationApp.swift
+++ SwiftUI-Navigation/SwiftUI_NavigationApp.swift
@@ -20,7 +20,8 @@ struct SwiftUI_NavigationApp: App {
                     .init(item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))),
                     .init(item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))),
                     .init(item: Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false))),
-                ]
+                ],
+                route: nil
             )
         )
         

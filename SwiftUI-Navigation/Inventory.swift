4e3a11efaaffad02a11161b1c8b7ff1a561a5011
Bruno Lopes <bruno.guitarpro@gmail.com>
2022-01-28 23:17:20 -0300

    fix for identation

--- SwiftUI-Navigation/Inventory.swift
+++ SwiftUI-Navigation/Inventory.swift
@@ -10,47 +10,47 @@ import IdentifiedCollections
 import CasePaths
 
 struct Item: Equatable, Identifiable {
-  let id = UUID()
-  var name: String
-  var color: Color?
-  var status: Status
-
-  enum Status: Equatable {
-    case inStock(quantity: Int)
-    case outOfStock(isOnBackOrder: Bool)
-
-    var isInStock: Bool {
-      guard case .inStock = self else { return false }
-      return true
-    }
-  }
-
-  struct Color: Equatable, Hashable {
+    let id: UUID = UUID()
     var name: String
-    var red: CGFloat = 0
-    var green: CGFloat = 0
-    var blue: CGFloat = 0
-
-    static var defaults: [Self] = [
-      .red,
-      .green,
-      .blue,
-      .black,
-      .yellow,
-      .white,
-    ]
-
-    static let red = Self(name: "Red", red: 1)
-    static let green = Self(name: "Green", green: 1)
-    static let blue = Self(name: "Blue", blue: 1)
-    static let black = Self(name: "Black")
-    static let yellow = Self(name: "Yellow", red: 1, green: 1)
-    static let white = Self(name: "White", red: 1, green: 1, blue: 1)
-
-    var swiftUIColor: SwiftUI.Color {
-        SwiftUI.Color(red: self.red, green: self.green, blue: self.blue)
+    var color: Color?
+    var status: Status
+    
+    enum Status: Equatable {
+        case inStock(quantity: Int)
+        case outOfStock(isOnBackOrder: Bool)
+        
+        var isInStock: Bool {
+            guard case .inStock = self else { return false }
+            return true
+        }
     }
-  }
+    
+    struct Color: Equatable, Hashable {
+        var name: String
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        static var defaults: [Self] = [
+            .red,
+            .green,
+            .blue,
+            .black,
+            .yellow,
+            .white,
+        ]
+        
+        static let red = Self(name: "Red", red: 1)
+        static let green = Self(name: "Green", green: 1)
+        static let blue = Self(name: "Blue", blue: 1)
+        static let black = Self(name: "Black")
+        static let yellow = Self(name: "Yellow", red: 1, green: 1)
+        static let white = Self(name: "White", red: 1, green: 1, blue: 1)
+        
+        var swiftUIColor: SwiftUI.Color {
+            SwiftUI.Color(red: self.red, green: self.green, blue: self.blue)
+        }
+    }
 }
 
 final class InventoryViewModel: ObservableObject {

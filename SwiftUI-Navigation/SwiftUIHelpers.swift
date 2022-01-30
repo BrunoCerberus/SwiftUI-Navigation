3779b937980d03c7c632dc6b7505ba29a2841ca5
Bruno Lopes <bruno.guitarpro@gmail.com>
2022-01-29 23:38:16 -0300

    moved LazyView to SwiftUIHelpers

--- SwiftUI-Navigation/SwiftUIHelpers.swift
+++ SwiftUI-Navigation/SwiftUIHelpers.swift
@@ -246,3 +246,13 @@ extension NavigationLink {
         )
     }
 }
+
+public struct LazyView<Content: View>: View {
+    private let build: () -> Content
+    public init(_ build: @autoclosure @escaping () -> Content) {
+        self.build = build
+    }
+    public var body: Content {
+        build()
+    }
+}
